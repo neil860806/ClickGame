114ecc39da5af50ad0d3b6f5ac264ea57abcaf08
4419-1-3\user <neil860806@gmail.com>
2017-12-23 11:56:56 +0800

    1. 完成healthComponent

---
+++ Assets/Scripts/View/Behavior/EnemyBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(MeshFader))]
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(healthComponent))]
+
+public class EnemyBehavior : MonoBehaviour {
+
+    private Animator animator;
+    private MeshFader meshFader;
+    private AudioSource audioSource;
+    public AudioClip hurtClip;
+    private healthComponent healthComponent;
+
+    private void Awake(){
+        animator = GetComponent<Animator>();
+        meshFader = GetComponent<MeshFader>();
+        audioSource = GetComponent<AudioSource>();
+        healthComponent = GetComponent<healthComponent>();
+    }
+
+    private void OnEnable() {
+        StartCoroutine(meshFader.FadeIn());
+        healthComponent.Init(100);
+    }
+
+    private void DoDamage(int attack) {
+        animator.SetTrigger("hurt");
+        audioSource.clip = hurtClip;
+        audioSource.Play();
+        healthComponent.Hurt(attack);
+    }
+
+    private void Update() {
+        if (healthComponent.IsOver)
+        
+            return;
+
+
+        if (Input.GetButtonDown("Fire1")) {
+            DoDamage(10);
+        }
+    }
+
+}
---
+++ Assets/Scripts/View/Component/healthComponent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthComponent : MonoBehaviour {
+
+    [SerializeField]
+    private Slider healthSlider;
+    private int currentHealth;
+    public float speed = 5f;
+
+    public bool IsOver {
+        get {
+            return currentHealth <= healthSlider.minValue;
+        }
+    }
+    [ContextMenu("Test Init 100")]
+    private void TestInit()
+    {
+        Init(100);
+    }
+    [ContextMenu("Test Hurt 50")]
+    private void TestHurt() {
+        Hurt(50);
+    }
+
+    public void Init(int maxHealth) {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = maxHealth;
+        currentHealth = maxHealth;
+    }
+
+    public void Hurt(int damage) {
+        currentHealth -= damage;
+        currentHealth = (int)Mathf.Max(currentHealth, healthSlider.minValue);
+    }
+
+    private void Update() {
+        healthSlider.value = Mathf.Lerp(healthSlider.value, currentHealth, Time.deltaTime * speed);
+    }
+}
